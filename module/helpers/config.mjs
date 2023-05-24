1d925cc3fbd10381644933df760b1ba2e6e5f857
rcottoson <rcottoson@gmail.com>
2023-05-24 13:45:54 -0600

    Add files via upload

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const CRICHTONIS = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ CRICHTONIS.abilities = {
+  "str": "CRICHTONIS.AbilityStr",
+  "dex": "CRICHTONIS.AbilityDex",
+  "con": "CRICHTONIS.AbilityCon",
+  "int": "CRICHTONIS.AbilityInt",
+  "wis": "CRICHTONIS.AbilityWis",
+  "cha": "CRICHTONIS.AbilityCha"
+};
+
+CRICHTONIS.abilityAbbreviations = {
+  "str": "CRICHTONIS.AbilityStrAbbr",
+  "dex": "CRICHTONIS.AbilityDexAbbr",
+  "con": "CRICHTONIS.AbilityConAbbr",
+  "int": "CRICHTONIS.AbilityIntAbbr",
+  "wis": "CRICHTONIS.AbilityWisAbbr",
+  "cha": "CRICHTONIS.AbilityChaAbbr"
+};
